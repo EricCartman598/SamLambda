9b9a99291a0ac25ecae6e3cfc9cfbe4fe9e40e61
EricCartman598 <morozov_spmt@mail.ru>
2021-08-08 19:18:07 +0300

    First commit

---
+++ src/test/java/com/techprimers/serverless/SpringCloudFunctionExampleApplicationTests.java
@@ -0,0 +1,27 @@
+package com.techprimers.serverless;
+
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudFunctionExampleApplicationTests {
+
+	@Test
+	public void successfulResponse() {
+		App app = new App();
+		APIGatewayProxyResponseEvent result = app.handleRequest(null, null);
+		assertEquals(result.getStatusCode().intValue(), 200);
+		assertEquals(result.getHeaders().get("Content-Type"), "application/json");
+		String content = result.getBody();
+		assertNotNull(content);
+		assertTrue(content.contains("\"message\""));
+		assertTrue(content.contains("\"hello from lambda\""));
+		assertTrue(content.contains("\"location\""));
+	}
+}
